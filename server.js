8df3e4b0b34a28e73381a78595bcd43e76f36b7e
vahide <vahidefarzane@gmail.com>
2024-03-04 20:15:24 +0330

    db changed

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () => {
+server.listen(4000, () => {
   console.log('JSON Server is running')
 })
